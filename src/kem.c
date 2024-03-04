f150a1c50105dabcb8be2ce9dca26c37937a5c13
dkostic <25055813+dkostic@users.noreply.github.com>
2024-03-04 11:58:07 -0800

    Porting changes from https://github.com/open-quantum-safe/liboqs/pull/1400 (#19)
    Co-authored-by: dkostic <dkostic@amazon.com>

--- src/kem.c
+++ src/kem.c
@@ -264,8 +264,8 @@ int crypto_kem_dec(OUT unsigned char *     ss,
   bike_memcpy(&l_ct, ct, sizeof(l_ct));
   bike_memcpy(&l_sk, sk, sizeof(l_sk));
 
-  // Decode and check if success.
-  volatile uint32_t success_cond = (decode(&e, &l_ct, &l_sk) == SUCCESS);
+  // Decode
+  decode(&e, &l_ct, &l_sk);
 
   // Copy the error vector in the padded struct.
   e_prime.val[0].val = e.val[0];
@@ -276,6 +276,7 @@ int crypto_kem_dec(OUT unsigned char *     ss,
   // Check if H(m') is equal to (e0', e1')
   // (in constant-time)
   GUARD(function_h(&e_tmp, &m_prime, &l_sk.pk));
+  uint32_t success_cond;
   success_cond = secure_cmp(PE0_RAW(&e_prime), PE0_RAW(&e_tmp), R_BYTES);
   success_cond &= secure_cmp(PE1_RAW(&e_prime), PE1_RAW(&e_tmp), R_BYTES);
 
